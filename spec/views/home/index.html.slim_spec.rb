d3d9f6bb2fe2f03d0fe673a48bb5d23309d63312
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2021-04-21 18:04:39 -0500

    add home

---
+++ spec/views/home/index.html.slim_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'home/index.html.slim', type: :view do
+  it 'displays the given text' do
+    render plain: 'This is directly rendered'
+
+    expect(rendered).to match(/directly rendered/)
+  end
+end
