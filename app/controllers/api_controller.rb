9c523768ebc05c074dd540bf075ad3b5528d2870
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2021-04-21 18:00:11 -0500

    generate api controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,7 @@
+# Api Controller
+class ApiController < ActionController::Base
+  def i18n
+    # locale param was defined in config/routes.rb
+    render file: "app/assets/i18n/#{params[:locale]}.json"
+  end
+end
