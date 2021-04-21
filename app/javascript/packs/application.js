aa412b8e568b66005b94c3c99ec8982fb2c8a7f9
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2021-04-21 17:08:44 -0500

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,28 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+import "@fortawesome/fontawesome-free/css/all"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+require('datatables.net-bs4')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import $ from 'jquery';
+import ApexCharts from 'apexcharts';
+import toastr from 'toastr'
+import Swal from 'sweetalert2'
+require('moment');
+
+global.$ = jQuery;
+window.Rails = Rails;
+global.toastr = toastr;
+global.Swal = Swal;
+window.ApexCharts = ApexCharts; // return apex chart
+import 'bootstrap'
+import appInit from './metronic/app.init';
+import "../../assets/stylesheets/application.scss"
+document.addEventListener('DOMContentLoaded', appInit);
