ade822b1901e0fb970f4668b976ff7b066c65f3a
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2021-04-21 18:15:38 -0500

    change index of erb to slim

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def toastr_flash
+    flash.each_with_object([]) do |(type, message), flash_messages|
+      type = alert_type(type)
+      puts 'Type:'
+      puts "Type:#{type}"
+      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
+      flash_messages << text.html_safe if message
+    end.join("\n").html_safe
+  end
+
+  def alert_type(type)
+    case (type)
+    when 'success'
+      'success'
+    when 'error'
+      'error'
+    when 'alert'
+      'warning'
+    else
+      'success'
+    end
+  end
 end
